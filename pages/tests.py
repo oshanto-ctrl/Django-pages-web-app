d66df452171f13ae88ad9c37b945228d60bd07d9
Rejoan Siddiky <siddikyrejoan@gmail.com>
2022-07-02 03:47:46 +0600

    added a contact section to contact developer and test for the template

--- pages/tests.py
+++ pages/tests.py
@@ -3,6 +3,10 @@ from django.test import SimpleTestCase
 from django.urls import reverse
 # Create your tests here.
 class HomePageTest(SimpleTestCase):
+    """
+    HomePageTest class inherits from SimpleTestCase
+    It's a model to test Home page of application.
+    """
     def test_url_exist_at_correct_location(self):
         respone = self.client.get("/")
         self.assertEqual(respone.status_code, 200)
@@ -13,6 +17,10 @@ class HomePageTest(SimpleTestCase):
 
 
 class AboutPageTest(SimpleTestCase):
+    """
+    AboutPageTest class inherits from SimpleTestCase
+    It's a model to test About page of application.
+    """
     def test_url_exist_at_correct_location(self):
         response = self.client.get("/about/")
         self.assertEqual(response.status_code, 200)
@@ -20,3 +28,20 @@ class AboutPageTest(SimpleTestCase):
     def test_template_name_correct(self):
         response = self.client.get(reverse("about"))
         self.assertTemplateUsed(response, "about.html")
+
+class ContactPageTest(SimpleTestCase):
+    """
+    ContactPageTest class inherits from SimpleTestCase
+    It's a model to test Contact page of application.
+    """
+    def test_url_exist_at_correct_location(self):
+        response = self.client.get("/contact/")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("contact"))
+        self.assertTemplateUsed(response, "contact.html")
+
+
+
+
--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,7 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
+
+
+class ContactPageView(TemplateView):
+    template_name = "contact.html"
